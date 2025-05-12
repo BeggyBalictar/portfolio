64bd3e2b906b0254bca345628f4890f995b5321a
Beggy Balictar <beggybalictar2@gmail.com>
2025-05-12 19:42:30 +0800

    Add js file

---
+++ main.js
@@ -0,0 +1,37 @@
+
+// Toggle menu for navigation
+function toggleMenu() {
+  var nav = document.getElementById("navLinks");
+  nav.classList.toggle("show");
+}
+
+// Star animation
+const starCount = 200;
+
+for (let i = 0; i < starCount; i++) {
+  const star = document.createElement("div");
+  star.classList.add("star");
+
+  star.style.left = Math.random() * 100 + "vw";
+
+  const size = Math.random() * 4 + 1;
+  star.style.width = size + "px";
+  star.style.height = size + "px";
+
+  star.style.animationDuration = (5 + Math.random() * 15) + "s";
+  star.style.animationDelay = Math.random() * 12 + "s";
+
+  document.body.appendChild(star);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const modal = new bootstrap.Modal(document.getElementById('certificateModal'));
+    const modalImage = document.getElementById('modalCertificateImage');
+
+    document.querySelectorAll('.certi-card img').forEach(img => {
+      img.addEventListener('click', () => {
+        modalImage.src = img.src;
+        modal.show();
+      });
+    });
+  });
